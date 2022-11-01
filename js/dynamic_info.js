593569a843b9cf754da045c048d5f5f1a324d350
pao <paoavalos16@gmail.com>
2022-11-01 02:24:20 +0200

    null id

--- js/dynamic_info.js
+++ js/dynamic_info.js
@@ -1,20 +1,20 @@
-fetch("https://api.npoint.io/b4c72af2537e4c2744c1").then((data)=>{
-    //console.log(data);
-    return data.json();
+    fetch("https://api.npoint.io/b2649a9dc1217bcf7e93").then((data)=>{
+        //console.log(data);
+        return data.json();
 
-}).then((complete_data)=>{
-    let data1="";
-    complete_data.map((values)=>{
-        data1= `<div class="post">
-      <img src=${values.image} alt="img" width="400" height="400" class="img">
-      <p>${values.text}</p>
-      
+    }).then((complete_data)=>{
+        let data1="";
+        complete_data.map((values)=>{
+            data1= `<div class="post">
+      <img class="img" src="../res/images/me.png" alt="icon" width="40" height="40">${values.Ptime}
+      <img class="img1" src=${values.img} alt="img" width="400" height="400">
+      <p>${values.texto}</p>
       <img class="img" src="../res/images/like.svg" alt="img" width="60" height="40"><br>
-    </div>`
+    </div>-->`
 
-    });
-    document.getElementById("post").innerHTML=data1;
-}).catch((err)=>{
-    console.log(err);
+        });
+        document.getElementById("main").innerHTML=data1;
+    }).catch((err)=>{
+        console.log(err);
 
-})
+    })
