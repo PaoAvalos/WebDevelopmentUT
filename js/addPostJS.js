a0573407ea3cf006390ec707bd66ce32143d6574
Iryna Hurova <iryna.gurova@.com>
2022-11-01 20:29:21 +0200

    added names to the posts and made it possible to post without image

--- js/addPostJS.js
+++ js/addPostJS.js
@@ -10,12 +10,20 @@ function readJSON() {
             posts.map(function (onePost) {
                 let post = document.createElement('div');
                 post.setAttribute('class', 'post');
-                post.innerHTML = `
-                    <img class="img" src="../res/images/me.png" alt="icon" width="40" height="40">${onePost.Ptime}
-                    <img class="img1" src=${onePost.img} alt="img" width="400" height="400">
-                    <p>${onePost.texto}</p>
-                    <img class="img" src="../res/images/like.svg" alt="img" width="60" height="40"><br>
-                `;
+                if(onePost.hasOwnProperty('img')) {
+                    post.innerHTML = `
+                        <img class="img" src="../res/images/me.png" alt="icon" width="40" height="40">${onePost.Ptime}
+                        <img class="img1" src=${onePost.img} alt="img" width="400" height="400">
+                        <p>${onePost.name}: ${onePost.texto}</p>
+                        <img class="img" src="../res/images/like.svg" alt="img" width="60" height="40"><br>
+                    `;
+                } else {
+                    post.innerHTML = `
+                        <img class="img" src="../res/images/me.png" alt="icon" width="40" height="40">${onePost.Ptime}
+                        <p>${onePost.name}: ${onePost.texto}</p>
+                        <img class="img" src="../res/images/like.svg" alt="img" width="60" height="40"><br>
+                    `;
+                }
                 document.getElementById('posts').appendChild(post);
 
             });
