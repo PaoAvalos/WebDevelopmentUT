d0a78ab47c5f5b7e67e600f12ac7f8eeb5e1da7a
hendrik <hendrikpyss@gmail.com>
2022-11-16 16:30:50 +0200

    added footer component

---
+++ hw3/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>
+        Contact us - ©PHI - Privacy Policy
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+    footer {
+    font-family: monospace;
+    font-weight: normal;
+    margin: 30px 30px 0px 30px;
+    padding: 20px;
+    background-color: darkcyan;
+    border-radius: 20px;
+    font-size: 150%;
+    color: black;
+}
+</style>
